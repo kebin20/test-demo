d9bb9db45434d5756668bc42bc44fbf772de8e4f
kebin20 <ktanzyl@hotmail.com>
2023-05-08 09:19:39 +0900

    feat: created test for calculator class

--- test/test-exercises.js
+++ test/test-exercises.js
@@ -2,9 +2,29 @@ function capitalize(str) {
   return str.toUpperCase();
 }
 
-
-
-function sum(a, b) {
-  return a + b;
+function reverseString(str) {
+  return str.split('').reverse().join('');
 }
-export { sum, capitalize };
+
+class Calculator {
+  add(a, b) {
+    return a + b;
+  }
+
+  subtract(a, b) {
+    return a - b;
+  }
+
+  divide(a, b) {
+    return a / b;
+  }
+
+  multiply(a, b) {
+    return a * b;
+  }
+}
+
+const calculator = new Calculator();
+
+
+export { capitalize, reverseString, calculator };
--- test/test-exercises.test.js
+++ test/test-exercises.test.js
@@ -1,12 +1,31 @@
-import {sum, capitalize} from './test-exercises';
-
-// Sum
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+import { capitalize, reverseString, calculator } from './test-exercises';
 
 // Capitalize
-test("capitalize hello world to Hello World", () => {
-  expect(capitalize("hello world")).toBe("HELLO WORLD");
+test('capitalize hello world to Hello World', () => {
+  expect(capitalize('hello world')).toBe('HELLO WORLD');
 });
 
+// Reverse
+test("reverse 'hello' to 'olleh'", () => {
+  expect(reverseString('hello')).toBe('olleh');
+});
+
+// Calculator: add
+test('adds 1 + 2 to equal 3', () => {
+  expect(calculator.add(1, 2)).toBe(3);
+});
+
+// Calculator: subtract
+test('subtract 3 - 2 to equal 1', () => {
+  expect(calculator.subtract(3, 2)).toBe(1);
+});
+
+// Calculator: multiply
+test('multiply 4 by 2 to equal 8', () => {
+  expect(calculator.multiply(4, 2)).toBe(8);
+});
+
+// Calculator: divide
+test('divide 4 by 2 to equal 2', () => {
+  expect(calculator.divide(4, 2)).toBe(2);
+});
